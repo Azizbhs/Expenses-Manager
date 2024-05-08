eb151ed22000fcdc976a25079e00ed80f86738ad
Mohamed Aziz Ben Hassen <MABENHAS@upv.edu.es>
2024-05-08 16:56:44 +0200

    first commit

---
+++ src/project2/Project2.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project2;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+/**
+ *
+ * @author GOATS
+ */
+public class Project2 extends Application{
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader= new FXMLLoader(getClass().getResource("/view/LogIn2.fxml"));
+        Parent root = loader.load();
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.setTitle("Expenses Manager");
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
