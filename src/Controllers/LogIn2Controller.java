c39a3fe6862f2203ab487a0e6bd9b2247e015487
Azizbhs <bhsaziz408@gmail.com>
2024-05-14 20:32:59 +0200

    sign up/ log in

--- src/Controllers/LogIn2Controller.java
+++ src/Controllers/LogIn2Controller.java
@@ -5,19 +5,63 @@
  */
 package Controllers;
 
+import java.io.IOException;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 
 public class LogIn2Controller implements Initializable {
+
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Button logInButton;
+    @FXML
+    private Button signupButton;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        BooleanBinding allFieldsFilled = Bindings.createBooleanBinding(() -> 
+      !usernameField.getText().isEmpty() && 
+    !passwordField.getText().isEmpty(), 
+    usernameField.textProperty(), 
+    passwordField.textProperty()
+);
+        logInButton.disableProperty().bind(allFieldsFilled.not());
     }    
     
+    @FXML
+    void signUp(ActionEvent event) throws IOException{
+      FXMLLoader miCargador = new FXMLLoader(getClass().getResource("/view/SignUp.fxml"));
+        Stage stage = new Stage();
+        Parent root = miCargador.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Sign Up");
+        stage.initModality(Modality.APPLICATION_MODAL);
+        //la ventana se muestra modal
+        stage.showAndWait();
+    }
+
+    @FXML
+    private void logIn(ActionEvent event) {
+    }
 }
--- src/Controllers/SignUpController.java
+++ src/Controllers/SignUpController.java
@@ -5,8 +5,15 @@
 package Controllers;
 
 import java.net.URL;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -15,12 +22,43 @@ import javafx.fxml.Initializable;
  */
 public class SignUpController implements Initializable {
 
+    @FXML
+    private TextField userNameField;
+    @FXML
+    private PasswordField passwordField2;
+    @FXML
+    private PasswordField repeatPassword;
+    @FXML
+    private TextField realName;
+    @FXML
+    private TextField email;
+    @FXML
+    private Button signUpButton;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        BooleanBinding allFieldsFilled = Bindings.createBooleanBinding(() -> 
+            !userNameField.getText().isEmpty() && 
+            !passwordField2.getText().isEmpty() && 
+            !repeatPassword.getText().isEmpty() && 
+            !realName.getText().isEmpty() && 
+            !email.getText().isEmpty(), 
+            userNameField.textProperty(), 
+            passwordField2.textProperty(), 
+            repeatPassword.textProperty(), 
+            realName.textProperty(), 
+            email.textProperty()
+        );
+
+        // Bind the disable property of the button to the negation of the BooleanBinding
+        signUpButton.disableProperty().bind(allFieldsFilled.not());
     }    
+
+    @FXML
+    private void signUp(ActionEvent event) {
+    }
     
 }
